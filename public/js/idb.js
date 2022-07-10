854f92334e0e90e3dbd08f7a7fdb924853e862fc
Cameron <sicklecj@gmail.com>
2022-07-10 11:39:42 -0500

    add psuedo code to idb.js for guidance

---
+++ public/js/idb.js
@@ -0,0 +1,11 @@
+//use module 18 Lesson 4 as advised in assignment details
+
+// input expense online
+// input deposit online
+//recv notification added expense
+//recv ntoificeation added deposit
+// input expense offline
+//input deposit offline
+// connection adds expense and deposits made offline
+
+//When finished, review mod 18 lesson 5 and 18.5.5 for mongoose validation and heroku deployment
